661222b9670894bb3e4cc64b3a4e4b6fd5414ab9
Taras Vyshegorodtsev <tasarvv@gmail.com>
2020-03-16 12:01:51 +0300

    fix: fix responsive design for  nav and types blocks

--- lesson_13/repair-design/js/main.js
+++ lesson_13/repair-design/js/main.js
@@ -1,15 +1,29 @@
-document.addEventListener("DOMContentLoaded", function(event){
-  // console.log ("klkl");
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const switchModal = () =>{
-    modal.classList.toggle('modal--visible');
-  };
-  const closeBtn = document.querySelector('.modal__close');
+// document.addEventListener("DOMContentLoaded", function(event){
+//   // console.log ("klkl");
+//   const modal = document.querySelector('.modal');
+//   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//   const switchModal = () =>{
+//     modal.classList.toggle('modal--visible');
+//   };
+//   const closeBtn = document.querySelector('.modal__close');
 
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
+//   modalBtn.forEach(element => {
+//     element.addEventListener('click', switchModal);
+//   });
+//   console.log (modal);
+//   closeBtn.addEventListener('click', switchModal);
+// });
+
+$(document).ready(function () {
+  var modal =$('.modal'),
+  modalBtn = $('[data-toggle="modal"]'),
+  closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
   });
-  console.log (modal);
-  closeBtn.addEventListener('click', switchModal);
+
+  closeBtn.on('click', function() {
+    modal.toggleClass('modal--visible');
+  });
 });
