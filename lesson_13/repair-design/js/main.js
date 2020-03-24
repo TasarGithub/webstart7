e64307adec827e9d1d5346d107a9dce8d70cfef0
Taras Vyshegorodtsev <tasarvv@gmail.com>
2020-03-24 15:41:30 +0300

    refact: refact animate in 24,25,26 lessons

--- lesson_13/repair-design/js/main.js
+++ lesson_13/repair-design/js/main.js
@@ -146,6 +146,10 @@ $(document).ready(function () {
       }
     }
   });
+  
+  $.validator.methods.email = function( value, element ) {
+    return this.optional( element ) || /[a-z]+@[a-z]+\.[a-z]+/.test( value );
+  };
 
   $('.footer__form').validate({
       errorClass: "invalid",
