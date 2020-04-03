7b930b3625d69f26d51330c09f827b0d3c32dcfd
Taras Vyshegorodtsev <tasarvv@gmail.com>
2020-04-03 21:56:21 +0300

    feat: 7-2 lesson flex

---
+++ lesson_7/lesson_7_2/sass/template/_delivery-pay__ copy.s
@@ -0,0 +1,91 @@
+.delivery-pay
+  display: flex
+  flex-direction: column
+  justify-content: start
+  // align-items: start
+  // flex-wrap: wrap
+  background-color: #f2f2f2
+  // border: 1px solid #1b1a1a
+  min-height: 408px
+  width: 100%
+  // margin: auto
+  &__title
+    flex-basis: 204px
+    display: flex
+    justify-content: center
+    align-items: center
+    // text-align: center
+    font-family: Tahoma
+    font-size: 30px
+    line-height: 27px
+    font-weight: 700
+    color: #333333
+    margin: 0
+    // margin-top: 89px
+    // margin-bottom: 91px
+  &__images
+    width: 100%
+    display: flex
+    justify-content: space-around
+    flex-wrap: wrap
+  &__names
+    margin-top: 10px
+    width: 100%
+    display: flex
+    justify-content: space-around//space-between
+    flex-wrap: wrap
+
+  &__img 
+    flex-basis: 25%
+    // display: block
+    margin: auto
+    max-width: 100%
+    // &:nth-child(1)
+    //   margin-right: 50px
+    //   // flex-basis: 23%
+    // &:nth-child(4)
+    //   margin-left: 50px
+    //   // flex-basis: 23%
+
+  &__name 
+    flex-basis: 25%
+    font-family: Arial
+    font-size: 18px
+    line-height: 22px
+    font-weight: 400
+    color: #333333
+    text-align: center
+    margin: auto
+    // &:nth-child(1)
+    //   margin-right: 50px
+    //   // flex-basis: 23%
+    // &:nth-child(4)
+    //   margin-left: 50px
+    //   // flex-basis: 23%
+
+
+    
+
+
+  // &__cards
+  //   width: 100%
+  //   // flex-basis: 200px
+  //   min-height: 204px
+  //   display: flex
+  //   justify-content: center
+  //   align-items: start
+  //   flex-wrap: wrap
+  //   // margin: auto
+  //   border: 2px solid #690d0d
+  // &__item
+  //   flex-basis: 25%
+  //   display: flex
+  //   justify-content: start
+  //   align-items: center
+  //   flex-direction: column
+  //   // width: 100%
+  //   flex-wrap: wrap
+  //   // width: 1366px
+  //   // height: 408px
+  //   background-color: #f2f2f2
+  //   border: 1.0px solid #ffffff
