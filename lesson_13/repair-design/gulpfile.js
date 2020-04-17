cb0ba71e467c0afbbe78465fc37ad94ae90f92c1
Taras Vyshegorodtsev <tasarvv@gmail.com>
2020-04-17 19:08:52 +0300

    feat: part-1 28 lesson, gulp, map

--- lesson_13/repair-design/gulpfile.js
+++ lesson_13/repair-design/gulpfile.js
@@ -1,10 +1,14 @@
-const {src, dest, watch} =  require('gulp');
+const {src, dest, watch,series} =  require('gulp');
 const browserSync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const wait = require('gulp-wait');
 const autoprefixer = require('gulp-autoprefixer');
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
+const tinypng = require('gulp-tinypng-compress');
+
 
 
 // Compile sass into CSS & auto-inject into browsers
@@ -51,8 +55,68 @@ function minifyCss() {
 //     }))
 //     .pipe(gulp.dest('dist/'));
 // });
+//
+
+function buildCSS(done) {
+  src('css/*.+(css|!min.css)')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    // .pipe(rename({
+    //   suffix: '.min'
+    // }))
+    .pipe(dest('dist/css/'));
+  done();
+}
+
+function buildJS(done) {
+  src(['js/**.js','!js/**.min.js'])
+    .pipe(minify({ext:{
+          min:'.js'
+        }
+     }))
+    .pipe(dest('dist/js/'));
+
+  src('js/**.min.js').pipe(dest('dist/js/'));
+  done();
+}
+function html(done) {
+  src('**html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest('dist/'));
+  done();
+}
+
+function php(done) {
+  src('**.php')
+    .pipe(dest('dist/'));
+  src('phpmailer/**/**')
+    .pipe(dest('dist/phpmailer'));
+  done();
+}
+
+function fonts(done) {
+  src('fonts/**/**')
+    .pipe(dest('dist/fonts'));
+  done();
+}
 
 
+// function imagemin(done) {
+//   src('img/**/**/*.{png,jpg,jpeg}')
+//     .pipe(tinypng({
+//       key: 'fVM45WFXJNjvNMff841nvZ7wzjFffNzp',
+//       sigFile: 'images/.tinypng-sigs',
+//       log: true
+//     }))
+//    .pipe(dest('dist/img'));
+
+//     src('img/**/**/*.svg').pipe(dest('dist/img'));
+
+//   done();
+// }
+
+
+//fVM45WFXJNjvNMff841nvZ7wzjFffNzp
 
 exports.serve = bs;
 exports.mini = minifyCss;
+exports.build = series(buildCSS, buildJS, html, php, fonts);//, imagemin);
