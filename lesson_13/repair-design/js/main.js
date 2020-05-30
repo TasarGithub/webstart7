e2e448acd2ca43ffb5200bb933234ae5a6de6687
Taras Vyshegorodtsev <tasarvv@gmail.com>
2020-05-30 14:19:51 +0300

    fix: response for modal, map, yuotube, design

--- lesson_13/repair-design/js/main.js
+++ lesson_13/repair-design/js/main.js
@@ -474,7 +474,7 @@ $(document).ready(function () {
   var player;
   $('.video__play').on('click', function onYouTubeIframeAPIReady(){
     player = new YT.Player('player', {
-      height: '465',
+      height: '100%',
       width: '100%',
       videoId: 'TvVYeLvujLk',
       events: {
